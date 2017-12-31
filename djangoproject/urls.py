d8ed393b940edc87c6e918ce0f2ce7d51f6cb5d4
deepfish77 <deepfish.77@gmail.com>
2017-12-31 21:43:20 +0200

    test

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from companies import views
+from rest_framework.urlpatterns import  format_suffix_patterns
 
 urlpatterns = [
     url(r'^$', include('posts.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include('posts.urls')),
+    url (r'^stocks/', views.StockList.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- posts/views.py
+++ posts/views.py
@@ -5,22 +5,22 @@ from .models import Posts
 
 # Create your views here.
 def index(request):
-  #return HttpResponse('HELLO FROM POSTS')
+    #return HttpResponse('HELLO FROM POSTS')
 
-  posts = Posts.objects.all()[:10]
+    posts = Posts.objects.all()[:10]
 
-  context = {
-    'title': 'Latest Posts',
-    'posts': posts
-  }
+    context = {
+        'title': 'Latest Posts',
+        'posts': posts
+        }
 
-  return render(request, 'posts/index.html', context)
+    return render(request, 'posts/index.html', context)
 
 def details(request, id):
-  post = Posts.objects.get(id=id)
+    post = Posts.objects.get(id=id)
 
-  context = {
-    'post': post
+    context = {
+        'post': post
   }
 
-  return render(request, 'posts/details.html', context)
+    return render(request, 'posts/details.html', context)
